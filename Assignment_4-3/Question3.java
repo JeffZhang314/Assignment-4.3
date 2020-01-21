0398c2a7239d95209a7815cec51a46b9489c06e8
JeffZhang314 <56049849+JeffZhang314@users.noreply.github.com>
2020-01-21 13:56:52 -0800

    Add files via upload

---
+++ Assignment_4-3/Question3.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+class Question3 {
+
+    public static void removeConsecutives(ArrayList<Integer> nums) {
+        //This method removes consecutive duplicate values from the 
+        //list nums.  Hint: loop backwards through the list
+        int numsIPlus = nums.get(nums.size() - 1);
+        for (int i = nums.size() - 2; i >= 0; i --) {
+            int numsI = nums.get(i);
+            if (numsI == numsIPlus) {
+                nums.remove(i);
+            } else {
+                numsIPlus = numsI;
+            }
+        }
+    }
+    
+    public static void main (String[] args) {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        nums.add(1);
+        nums.add(1);
+        nums.add(2);
+        nums.add(2);
+        nums.add(2);
+        nums.add(2);
+        nums.add(3);
+        nums.add(3);
+        nums.add(1);
+        System.out.println("Our list before: " + nums);
+        removeConsecutives(nums); 
+        System.out.println("Our list after:  " + nums);
+        //should output [1,2,3,1]
+    }
+
+    
+}
